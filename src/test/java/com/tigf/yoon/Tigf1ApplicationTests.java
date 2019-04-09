eb1f15bdaf7068658c8bf38a7def626c572ecc80
YOON <yoonz44@naver.com>
2019-04-09 16:21:48 +0900

    테스트 코드 나중에2

--- src/test/java/com/tigf/yoon/Tigf1ApplicationTests.java
+++ src/test/java/com/tigf/yoon/Tigf1ApplicationTests.java
@@ -1,12 +1,16 @@
 package com.tigf.yoon;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.tigf.yoon.domain.MainController;
-
 @RunWith(SpringRunner.class)
-@WebMvcTest(MainController.class)
+@SpringBootTest
 public class Tigf1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
 }
