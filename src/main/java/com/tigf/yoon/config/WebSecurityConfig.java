b5d2d16cccf63319223007afaf9534c356e7398a
YOON <yoonz44@naver.com>
2019-04-09 16:17:53 +0900

    빌드 애러 4

--- src/main/java/com/tigf/yoon/config/WebSecurityConfig.java
+++ src/main/java/com/tigf/yoon/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.tigf.yoon.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -15,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableJpaAuditing
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
