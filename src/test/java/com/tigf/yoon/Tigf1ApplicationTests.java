4b297fbf9d08e496633ca87915713b52b9651674
YOON <yoonz44@naver.com>
2019-04-09 16:20:10 +0900

    테스트 코드 나중에하자

--- src/test/java/com/tigf/yoon/Tigf1ApplicationTests.java
+++ src/test/java/com/tigf/yoon/Tigf1ApplicationTests.java
@@ -1,31 +1,12 @@
 package com.tigf.yoon;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.tigf.yoon.domain.MainController;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MainController.class)
 public class Tigf1ApplicationTests {
-
-	@Autowired
-	private MockMvc mvc;
-	
-	@Test
-	public void givenRequestOnPublicPage_showReturn200() throws Exception {
-		mvc.perform(get("/")).andExpect(status().isOk());
-	}
-
-	@Test
-	public void givenRequestOnPrivatePageWithoutLogin_shouldReturn401() throws Exception {
-		mvc.perform(get("/main")).andExpect(status().isUnauthorized());
-	}
 }
